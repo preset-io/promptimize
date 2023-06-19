aa6b76fab440f02139332aaf0f365631e8ef9b9c
Maxime Beauchemin <maximebeauchemin@gmail.com>
2023-06-19 12:58:17 -0700

    minor tweak to publish.sh

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@
 # FIRST! manually update the setup.py with the version number
 # export PROMPTIMIZE_VERSION=0.2.3
 
+git checkout main
 git commit -a -m "Version $PROMPTIMIZE_VERSION"
 git tag $PROMPTIMIZE_VERSION
-git push origin $PROMPTIMIZE_VERSION
+git push origin main $PROMPTIMIZE_VERSION
 python setup.py sdist bdist_wheel
 twine upload dist/promptimize-$PROMPTIMIZE_VERSION*
